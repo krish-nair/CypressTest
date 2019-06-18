1910983c1d8f8a057a6d073e5cb4e2c19976d603
krish-nair <krish.nair101@gmail.com>
2019-06-18 21:52:55 +1200

    login scenario test

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -2,5 +2,9 @@ context("Cypress login-demo", () => {
   it("login-scenario", () => {
     cy.visit("http://automationpractice.com/index.php");
     cy.get(".login").click();
+    cy.get("#email").type("test@gmail.com");
+    cy.get("#passwd").type("12345");
+    cy.get("#SubmitLogin > span").click();
+    cy.title().should("include", "My account - My Store");
   });
 });
